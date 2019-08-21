86055436f82278f1fb77ad4aae26607de5b86f6c
Rob Potter <robert.potter@my.jcu.edu.au>
2019-08-21 22:57:21 +1000

    Refactor broken score program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,12 +1,22 @@
 """Broken Score Program (fixed)
 """
 
-score = float(input("Enter score: "))
-if 50 <= score < 90:
-    print("Passable")
-elif 90 <= score <= 100:
-    print("Excellent")
-elif 0 <= score < 50:
-    print("Bad")
-else:
-    print("Invalid score")
+
+def main():
+    score = get_score()
+    print(score)
+
+
+def get_score():
+    score = float(input("Enter score: "))
+    if 50 <= score < 90:
+        return "Passable"
+    elif 90 <= score <= 100:
+        return "Excellent"
+    elif 0 <= score < 50:
+        return "Bad"
+    else:
+        return "Invalid score"
+
+
+main()
