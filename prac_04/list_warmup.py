d67001bd3fe13673870b25d88df1775233d79db7
Rob Potter <robert.potter@my.jcu.edu.au>
2019-08-27 10:40:53 +1000

    Warm-Up questions

---
+++ prac_04/list_warmup.py
@@ -0,0 +1,24 @@
+""" Prac 04 Warm-Up
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# What values do the following expressions have?
+# answer = numbers[0]           # 3
+# answer = numbers[-1]          # 2
+# answer = numbers[3]           # 1
+# answer = numbers[:-1]         # [3, 1, 4, 1, 5, 9]
+# answer = numbers[3:4]         # [1]
+# answer = 5 in numbers         # True
+# answer = 7 in numbers         # False
+# answer = '3' in numbers       # False
+# answer = numbers + [6, 5, 3]  # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Write Python expressions to achieve the following:
+# numbers[0] = "ten"        # 1. Change the first element of numbers to "ten"
+# numbers[-1] = 1           # 2. Change the last element of numbers to 1
+# answer = numbers[2:]      # 3. Get all the elements from numbers except the first two
+# answer = 9 in numbers     # 4. Check if 9 is an element of numbers
+
+# print(answer)
+# print(numbers)
