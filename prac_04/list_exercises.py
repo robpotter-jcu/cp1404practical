5da1b89d912c4698ca69290e623885855561d6a6
Rob Potter <robert.potter@my.jcu.edu.au>
2019-08-28 12:40:08 +1000

    Add woefully inadequate security ckecker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -14,7 +14,23 @@ def main():
     print("The last number is {}".format(numbers[-1]))
     print("The smallest number is {}".format(min(numbers)))
     print("The largest number is {}".format(max(numbers)))
-    print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+    print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
+
+    username = input("Username: ")
+    access_outcome = security_checker(username)
+    print(access_outcome)
+
+
+def security_checker(username):
+    """Woefully inadequate security checker"""
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+                 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+                 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+    if username in usernames:
+        return "Access granted"
+    else:
+        return "Access denied"
 
 
 main()
