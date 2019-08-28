8f9f160e4618199229d93653e931afa6e919cbda
Rob Potter <robert.potter@my.jcu.edu.au>
2019-08-28 12:17:07 +1000

    Basic list operations

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,20 @@
+"""
+CP1404 Practical 04
+List exercises
+"""
+
+
+def main():
+    """Basic list operations"""
+    numbers = []
+    for i in range(5):
+        numbers.append(int(input("Number: ")))
+
+    print("The first number is {}".format(numbers[0]))
+    print("The last number is {}".format(numbers[-1]))
+    print("The smallest number is {}".format(min(numbers)))
+    print("The largest number is {}".format(max(numbers)))
+    print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+
+
+main()
